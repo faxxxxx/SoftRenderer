30d2386f553d8b5a6ebc23c318849bd8ffd0d836
faxxxxx <83041355@qq.com>
2018-10-24 15:07:29 +0800

    commit

--- SoftRenderer/src/Core/Base/Matrix.h
+++ SoftRenderer/src/Core/Base/Matrix.h
@@ -1,17 +1,19 @@
 #pragma once
 #include "BaseType.h"
 #include <memory.h>
+#include <math.h>
+#include <fstream>
 
 class Matrix4x4f {
  public:
 	float _data[16];
-    
-    // 0  1  2  3
-    // 4  5  6  7
-    // 8  9  10 11
-    // 12 13 14 15
+
+	// 0  1  2  3
+	// 4  5  6  7
+	// 8  9  10 11
+	// 12 13 14 15
 	Matrix4x4f() {
-        LoadIdentity();
+		LoadIdentity();
 	}
 
 	float& Get(int row, int column) {
@@ -26,37 +28,34 @@ class Matrix4x4f {
 	float& operator [] (int index) {
 		return _data[index];
 	}
-    void LoadIdentity() {
-        memset(_data, 0, sizeof(_data));
-        _data[0] = _data[5] = _data[10] = _data[15] = 1.0f;
-    }
-    
-    void Transpose()
-    {
-        std::swap(_data[1], _data[4]);
-        std::swap(_data[2], _data[8]);
-        std::swap(_data[3], _data[12]);
-        std::swap(_data[6], _data[9]);
-        std::swap(_data[7], _data[13]);
-        std::swap(_data[11], _data[14]);
-    }
-    
-    void Inverse()
-    {
-        
-    }
-    
-    void RotateAxixY(float angle)
-    {
-        _data[0] = cos(Angle2Rad(angle));
-        _data[2] = sin(Angle2Rad(angle));
-        _data[8] = -_data[2];
-        _data[10] = _data[0];
-    }
-    
+	void LoadIdentity() {
+		memset(_data, 0, sizeof(_data));
+		_data[0] = _data[5] = _data[10] = _data[15] = 1.0f;
+	}
+
+	void Transpose() {
+		std::swap(_data[1], _data[4]);
+		std::swap(_data[2], _data[8]);
+		std::swap(_data[3], _data[12]);
+		std::swap(_data[6], _data[9]);
+		std::swap(_data[7], _data[13]);
+		std::swap(_data[11], _data[14]);
+	}
+
+	void Inverse() {
+
+	}
+
+	void RotateAxixY(float angle) {
+		_data[0] = cos(Angle2Rad(angle));
+		_data[2] = sin(Angle2Rad(angle));
+		_data[8] = -_data[2];
+		_data[10] = _data[0];
+	}
+
 	Vector4f MultiplyVector4(const Vector4f& inV) const;
 	void MultiplyVector4(const Vector4f& inV, Vector4f& output) const;
-    Matrix4x4f MultiplyMatrix4x4(const Matrix4x4f& mat) const;
+	Matrix4x4f MultiplyMatrix4x4(const Matrix4x4f& mat) const;
 };
 
 inline void Matrix4x4f::MultiplyVector4(const Vector4f & inV, Vector4f & output) const {
@@ -67,39 +66,37 @@ inline void Matrix4x4f::MultiplyVector4(const Vector4f & inV, Vector4f & output)
 	output.w = _data[12] * inV.x + _data[13] * inV.y + _data[14] * inV.z + _data[15] * inV.w;
 }
 
-inline Vector4f Matrix4x4f::MultiplyVector4(const Vector4f& inV) const
-{
-    Vector4f ret;
-    MultiplyVector4(inV, ret);
-    return ret;
+inline Vector4f Matrix4x4f::MultiplyVector4(const Vector4f& inV) const {
+	Vector4f ret;
+	MultiplyVector4(inV, ret);
+	return ret;
 }
 
-inline Matrix4x4f Matrix4x4f::MultiplyMatrix4x4(const Matrix4x4f& mat) const
-{
-    // 0  1  2  3
-    // 4  5  6  7
-    // 8  9  10 11
-    // 12 13 14 15
-    auto& data2 = mat._data;
-    Matrix4x4f ret;
-    ret[0] = _data[0] * data2[0] + _data[1] * data2[4] + _data[2] * data2[8] + _data[3] * data2[12];
-    ret[1] = _data[0] * data2[1] + _data[1] * data2[5] + _data[2] * data2[9] + _data[3] * data2[13];
-    ret[2] = _data[0] * data2[2] + _data[1] * data2[6] + _data[2] * data2[10] + _data[3] * data2[14];
-    ret[3] = _data[0] * data2[3] + _data[1] * data2[7] + _data[2] * data2[11] + _data[3] * data2[15];
-    
-    ret[4] = _data[4] * data2[0] + _data[5] * data2[4] + _data[6] * data2[8] + _data[7] * data2[12];
-    ret[5] = _data[4] * data2[1] + _data[5] * data2[5] + _data[6] * data2[9] + _data[7] * data2[13];
-    ret[6] = _data[4] * data2[2] + _data[5] * data2[6] + _data[6] * data2[10] + _data[7] * data2[14];
-    ret[7] = _data[4] * data2[3] + _data[5] * data2[7] + _data[6] * data2[11] + _data[7] * data2[15];
+inline Matrix4x4f Matrix4x4f::MultiplyMatrix4x4(const Matrix4x4f& mat) const {
+	// 0  1  2  3
+	// 4  5  6  7
+	// 8  9  10 11
+	// 12 13 14 15
+	auto& data2 = mat._data;
+	Matrix4x4f ret;
+	ret[0] = _data[0] * data2[0] + _data[1] * data2[4] + _data[2] * data2[8] + _data[3] * data2[12];
+	ret[1] = _data[0] * data2[1] + _data[1] * data2[5] + _data[2] * data2[9] + _data[3] * data2[13];
+	ret[2] = _data[0] * data2[2] + _data[1] * data2[6] + _data[2] * data2[10] + _data[3] * data2[14];
+	ret[3] = _data[0] * data2[3] + _data[1] * data2[7] + _data[2] * data2[11] + _data[3] * data2[15];
 
-    ret[8] = _data[8] * data2[0] + _data[9] * data2[4] + _data[10] * data2[8] + _data[11] * data2[12];
-    ret[9] = _data[8] * data2[1] + _data[9] * data2[5] + _data[10] * data2[9] + _data[11] * data2[13];
-    ret[10] = _data[8] * data2[2] + _data[9] * data2[6] + _data[10] * data2[10] + _data[11] * data2[14];
-    ret[11] = _data[8] * data2[3] + _data[9] * data2[7] + _data[10] * data2[11] + _data[11] * data2[15];
+	ret[4] = _data[4] * data2[0] + _data[5] * data2[4] + _data[6] * data2[8] + _data[7] * data2[12];
+	ret[5] = _data[4] * data2[1] + _data[5] * data2[5] + _data[6] * data2[9] + _data[7] * data2[13];
+	ret[6] = _data[4] * data2[2] + _data[5] * data2[6] + _data[6] * data2[10] + _data[7] * data2[14];
+	ret[7] = _data[4] * data2[3] + _data[5] * data2[7] + _data[6] * data2[11] + _data[7] * data2[15];
 
-    ret[12] = _data[12] * data2[0] + _data[13] * data2[4] + _data[14] * data2[8] + _data[15] * data2[12];
-    ret[13] = _data[12] * data2[1] + _data[13] * data2[5] + _data[14] * data2[9] + _data[15] * data2[13];
-    ret[14] = _data[12] * data2[2] + _data[13] * data2[6] + _data[14] * data2[10] + _data[15] * data2[14];
-    ret[15] = _data[12] * data2[3] + _data[13] * data2[7] + _data[14] * data2[11] + _data[15] * data2[15];
-    return ret;
+	ret[8] = _data[8] * data2[0] + _data[9] * data2[4] + _data[10] * data2[8] + _data[11] * data2[12];
+	ret[9] = _data[8] * data2[1] + _data[9] * data2[5] + _data[10] * data2[9] + _data[11] * data2[13];
+	ret[10] = _data[8] * data2[2] + _data[9] * data2[6] + _data[10] * data2[10] + _data[11] * data2[14];
+	ret[11] = _data[8] * data2[3] + _data[9] * data2[7] + _data[10] * data2[11] + _data[11] * data2[15];
+
+	ret[12] = _data[12] * data2[0] + _data[13] * data2[4] + _data[14] * data2[8] + _data[15] * data2[12];
+	ret[13] = _data[12] * data2[1] + _data[13] * data2[5] + _data[14] * data2[9] + _data[15] * data2[13];
+	ret[14] = _data[12] * data2[2] + _data[13] * data2[6] + _data[14] * data2[10] + _data[15] * data2[14];
+	ret[15] = _data[12] * data2[3] + _data[13] * data2[7] + _data[14] * data2[11] + _data[15] * data2[15];
+	return ret;
 }
--- SoftRenderer/src/Core/Graphic/Texture/Texture.cpp
+++ SoftRenderer/src/Core/Graphic/Texture/Texture.cpp
@@ -14,111 +14,101 @@
 
 #define PNG_READ_HEADER 4
 
-int read_chunk_callback(png_structp ptr,png_unknown_chunkp chunk)
-{
-    return 1;
+int read_chunk_callback(png_structp ptr,png_unknown_chunkp chunk) {
+	return 1;
 }
 
-void read_row_callback(png_structp ptr,png_uint_32 row,int pass)
-{
-    
+void read_row_callback(png_structp ptr,png_uint_32 row,int pass) {
+
 }
 
-int png_read(const char* filename, png_datap data)
-{
-    FILE *fp = fopen(filename,"rb");
-    if(fp == NULL) return PNG_ERROR_NO_FILE;
-    
-    fseek(fp,0,SEEK_END);
-    int fSize = ftell(fp);
-    rewind(fp);
-    unsigned char* header = (unsigned char*)malloc(PNG_READ_HEADER);
-    fread(header,1,PNG_READ_HEADER,fp);
-    int is_png = !png_sig_cmp(header,0,PNG_READ_HEADER);
-    if(!is_png) return PNG_ERROR_NOT_PNG;
-    
-    png_structp png_ptr;
-    png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING,NULL, NULL, NULL);
-    if(!png_ptr) return PNG_ERROR_CREATE_PNG_STRUCT;
-    
-    png_infop info_ptr;
-    info_ptr = png_create_info_struct(png_ptr);
-    if(!info_ptr)
-    {
-        printf("create info struct failed!\n");
-        png_destroy_read_struct(&png_ptr,NULL,NULL);
-        return PNG_ERROR_CREATE_INFO_STRUCT;
-    }
-    png_infop end_info;
-    end_info = png_create_info_struct(png_ptr);
-    if(!end_info)
-    {
-        printf("create end info failed!\n");
-        png_destroy_read_struct(&png_ptr,NULL,NULL);
-        return PNG_ERROR_CREATE_INFO_STRUCT;
-    }
-    if(setjmp(png_jmpbuf(png_ptr)))
-    {
-        png_destroy_read_struct(&png_ptr,&info_ptr,&end_info);
-        return PNG_ERROR_JMP;
-    }
-    png_init_io(png_ptr,fp);
-    png_set_sig_bytes(png_ptr,PNG_READ_HEADER);
-    // png_voidp user_chunk_ptr = png_get_user_chunk_ptr(png_ptr);
-    // png_set_read_user_chunk_fn(png_ptr,user_chunk_ptr,read_chunk_callback);
-    // png_set_keep_unknown_chunks(png_ptr,PNG_HANDLE_CHUNK_NEVER,NULL,0);
-    
-    //png_set_read_status_fn(png_ptr,read_row_callback);
-    
-    //low lever read
-    png_read_info(png_ptr,info_ptr);
-    png_uint_32 width;
-    png_uint_32 height;
-    int bit_depth;
-    int color_type;
-    png_get_IHDR(png_ptr,info_ptr,&width,&height,&bit_depth,&color_type,NULL,NULL,NULL);
-    // printf("width:%d,height:%d,bit_depth:%d,color_type:%d",width,height,bit_depth,color_type);
-    
-    png_bytep raw_data = (png_bytep)malloc(width*height*4*sizeof(png_byte));
-    // printf("row bytes:%d\n",png_get_rowbytes(png_ptr,info_ptr));
-    
-    png_bytep *row_pointers = (png_bytep*)malloc(height*sizeof(png_bytep));
-    for(int i=0;i<height;i++)
-    {
-        row_pointers[i] = raw_data+ i* (width * 4);
-    }
-    png_read_image(png_ptr,row_pointers);
-    
-    data->width = width;
-    data->height = height;
-    data->data = (unsigned char*)raw_data;
-    // printf("png_read_success\n");
-    
-    //clean
-    fclose(fp);
-    png_destroy_read_struct(&png_ptr,&info_ptr,&end_info);
-    free(row_pointers);
-    
-    return PNG_READ_SUCCESS;
+int png_read(const char* filename, png_datap data) {
+	FILE *fp = fopen(filename,"rb");
+	if(fp == NULL) return PNG_ERROR_NO_FILE;
+
+	fseek(fp,0,SEEK_END);
+	int fSize = ftell(fp);
+	rewind(fp);
+	unsigned char* header = (unsigned char*)malloc(PNG_READ_HEADER);
+	fread(header,1,PNG_READ_HEADER,fp);
+	int is_png = !png_sig_cmp(header,0,PNG_READ_HEADER);
+	if(!is_png) return PNG_ERROR_NOT_PNG;
+
+	png_structp png_ptr;
+	png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING,NULL, NULL, NULL);
+	if(!png_ptr) return PNG_ERROR_CREATE_PNG_STRUCT;
+
+	png_infop info_ptr;
+	info_ptr = png_create_info_struct(png_ptr);
+	if(!info_ptr) {
+		printf("create info struct failed!\n");
+		png_destroy_read_struct(&png_ptr,NULL,NULL);
+		return PNG_ERROR_CREATE_INFO_STRUCT;
+	}
+	png_infop end_info;
+	end_info = png_create_info_struct(png_ptr);
+	if(!end_info) {
+		printf("create end info failed!\n");
+		png_destroy_read_struct(&png_ptr,NULL,NULL);
+		return PNG_ERROR_CREATE_INFO_STRUCT;
+	}
+	if(setjmp(png_jmpbuf(png_ptr))) {
+		png_destroy_read_struct(&png_ptr,&info_ptr,&end_info);
+		return PNG_ERROR_JMP;
+	}
+	png_init_io(png_ptr,fp);
+	png_set_sig_bytes(png_ptr,PNG_READ_HEADER);
+	// png_voidp user_chunk_ptr = png_get_user_chunk_ptr(png_ptr);
+	// png_set_read_user_chunk_fn(png_ptr,user_chunk_ptr,read_chunk_callback);
+	// png_set_keep_unknown_chunks(png_ptr,PNG_HANDLE_CHUNK_NEVER,NULL,0);
+
+	//png_set_read_status_fn(png_ptr,read_row_callback);
+
+	//low lever read
+	png_read_info(png_ptr,info_ptr);
+	png_uint_32 width;
+	png_uint_32 height;
+	int bit_depth;
+	int color_type;
+	png_get_IHDR(png_ptr,info_ptr,&width,&height,&bit_depth,&color_type,NULL,NULL,NULL);
+	// printf("width:%d,height:%d,bit_depth:%d,color_type:%d",width,height,bit_depth,color_type);
+
+	png_bytep raw_data = (png_bytep)malloc(width*height*4*sizeof(png_byte));
+	// printf("row bytes:%d\n",png_get_rowbytes(png_ptr,info_ptr));
+
+	png_bytep *row_pointers = (png_bytep*)malloc(height*sizeof(png_bytep));
+	for(int i=0; i<height; i++) {
+		row_pointers[i] = raw_data+ i* (width * 4);
+	}
+	png_read_image(png_ptr,row_pointers);
+
+	data->width = width;
+	data->height = height;
+	data->data = (unsigned char*)raw_data;
+	// printf("png_read_success\n");
+
+	//clean
+	fclose(fp);
+	png_destroy_read_struct(&png_ptr,&info_ptr,&end_info);
+	free(row_pointers);
+
+	return PNG_READ_SUCCESS;
 }
 
-void Texture::Load(const std::string &path)
-{
-    png_read(path.c_str(), _data);
+void Texture::Load(const std::string &path) {
+	png_read(path.c_str(), _data);
 }
 
-Color Texture::GetColor(const Vector2f &uv)
-{
-    Color ret;
-    if (_data)
-    {
-        int row = _data->height - (int)(uv.y * (_data->height - 1)) - 1;
-        int col = (int)(uv.x * (_data->width - 1));
-        int idx = row * _data->width * 4 + col * 4;
-        ret.r = _data->data[idx] / 255.0f;
-        ret.g = _data->data[idx+1] / 255.0f;
-        ret.b = _data->data[idx+2] / 255.0f;
-        ret.a = 1.0f;
-    }
-    return ret;
+Color Texture::GetColor(const Vector2f &uv) {
+	Color ret;
+	if (_data) {
+		int row = _data->height - (int)(uv.y * (_data->height - 1)) - 1;
+		int col = (int)(uv.x * (_data->width - 1));
+		int idx = row * _data->width * 4 + col * 4;
+		ret.r = _data->data[idx] / 255.0f;
+		ret.g = _data->data[idx+1] / 255.0f;
+		ret.b = _data->data[idx+2] / 255.0f;
+		ret.a = 1.0f;
+	}
+	return ret;
 }
